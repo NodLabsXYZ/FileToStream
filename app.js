bd5248ede15cc7957904b4f091d52b2492bfacfe
Jared Cosulich <jared.cosulich@gmail.com>
2022-02-17 11:16:46 -0500

    use tail

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const Tail = require('tail').Tail;
-const RocketTurtleTail = require('rocket-turtle-tail');
+const Tail = require('tail').Tail;
 const SSEChannel = require('sse-pubsub');
 
 const app = express();
@@ -9,38 +8,15 @@ app.use(cors());
 
 const channel = new SSEChannel();
 
-const tail = new RocketTurtleTail('../log.txt');
+const tail = new Tail("../log.txt");
 
-tail.on('line', (line) => {
-  console.log('OH LOOK, A LINE!', line);
+tail.on("line", function(data) {
+  channel.publish(data.toString(), 'log')
 });
 
-tail.on('error', (err) => {
-  console.log('Boooooo an error:', err);
+tail.on("error", function(error) {
+  console.log('ERROR: ', error);
 });
-
-tail.start();
-
-// const tail = new Tail("../log.txt");
-
-// tail.on("line", function(data) {
-//   console.log("Line: " + data);
-//   channel.publish(data.toString(), 'log')
-// });
-
-// tail.on("error", function(error) {
-//   console.log('ERROR: ', error);
-// });
-
-// const stream = TailingReadableStream.createReadStream("../log.txt", {timeout: 0});
-
-// stream.on('data', buffer => {
-//   channel.publish(buffer.toString(), 'log')
-// });
-
-// stream.on('close', () => {
-//   console.log("close");
-// });
 
 app.get('/stream', (req, res) => channel.subscribe(req, res));
 
