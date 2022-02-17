bdb8a533159ffa7a3d4abd0d2322c9b4eb7a2b83
Jared Cosulich <jared.cosulich@gmail.com>
2022-02-17 14:39:29 -0500

    ssl experiment

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+
+const fs = require('fs');
+const https = require('https');
+
 const Tail = require('tail').Tail;
 const SSEChannel = require('sse-pubsub');
 
 const app = express();
 app.use(cors());
+
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
+
+const httpsServer = https.createServer(credentials, app);
 
 const channel = new SSEChannel();
 
@@ -20,5 +31,4 @@ tail.on("error", function(error) {
 
 app.get('/stream', (req, res) => channel.subscribe(req, res));
 
-app.listen(3005);
-
+httpsServer.listen(8443);
